93a67a1872c38271b6436c0425a84bbef44853d9
HyeongSeok Kim <king9833@korea.ac.kr>
2024-02-20 17:29:57 +0900

    과제제출용 누나 API

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const API_KEY = '1078bb37e6b440308bd6c8e562d0d5c6';
+/////const API_KEY = '1078bb37e6b440308bd6c8e562d0d5c6';
 let news = []; // 뉴스는 자주 쓰이는 애이므로 전역변수.
+
+let keyword = "테슬라";
+let PAGE_SIZE = 10;
 
 const getLatesNews = async ()=>{ // 에이싱크 - 비동기
     // URL 이라는 인스턴스. JS에서 상당히 많은 기능 제공해줄것임.
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    /////const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+
+    // 도메인 제출용 누나 API.
+    const url = new URL(`https://gogumi-news.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`);
     const response = await fetch(url);
     const data = await response.json(); // body에 있는 것은 json형태로 받아와줘야 한다.
     news = data.articles // 재할당만.
